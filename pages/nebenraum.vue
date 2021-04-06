a452f693110b5a7967a266c0245d805173746dcd
Felix Deiters <messismore@users.noreply.github.com>
2021-04-06 18:48:32 +0200

    Add nebenraum.vue

---
+++ pages/nebenraum.vue
@@ -0,0 +1,200 @@
+<template>
+  <AppProject name="Gesellschaft der Nebenräume" author="Tobias Michnik">
+    <template #header>
+      <div>
+        <h3>Untertitel: Versuch einer Neuordnung der ausgelagerten Dinge</h3>
+        <p>
+          Ein formales Spiel, entwachsen aus der eigenen Affinität zu
+          Nebenräumen. Vorgefundenes im Keller eines Berliner Mietshauses anders
+          übersetzen. Nicht verbessern, bloß Bestehendes präzise begreifen, neu
+          ordnen und anders erzählen. Räumen mit Dingen eine eigenständige
+          Bedeutung geben. Die Betrachtung der Dinge durch den Raum verändern.
+        </p>
+        <p>
+          Ein formales Spiel, entwachsen aus der eigenen Affinität zu
+          Nebenräumen. Vorgefundenes im Keller eines Berliner Mietshauses anders
+          übersetzen. Nicht verbessern, bloß Bestehendes präzise begreifen, neu
+          ordnen und anders erzählen. Räumen mit Dingen eine eigenständige
+          Bedeutung geben. Die Betrachtung der Dinge durch den Raum verändern.
+        </p>
+        <ul>
+          <li>Regeln der Gestaltung ableiten,</li>
+          <li>Konstruktionsprinzipien adaptieren,</li>
+          <li>Dinge sortieren,</li>
+          <li>Ordnung definieren,</li>
+          <li>Räume entwerfen, dabei:</li>
+          <li>verzerren, überhöhen, spekulieren.</li>
+        </ul>
+      </div>
+    </template>
+    <template #main>
+      <AppBox width="medium">
+        <p>
+          Nebenräume sind keine vordergründigen Repräsentationsräume. Sie
+          enthalten Verdecktes, Verborgenes, Ausgelagertes, aber sind zugleich
+          auch genau deshalb: Ermöglichungsräume. Räume die aufgrund ihres
+          Verhältnissesses zum Hauptraum bestimmte Rollen einnehmen. Bei
+          Nebenräumen geht es um Dinge und Anschlüsse, aber auch um Programme
+          und Aktivitäten, die anderswo keinen Platz finden (sollen).
+        </p>
+      </AppBox>
+      <AppBox width="small">
+        <p>
+          Nebenräume lassen sich nicht losgelöst von den Dingen betrachten.
+          Exemplarisch hierzu drei historische Beispiele, die zwar nicht
+          explizit Nebenraum sind, aber dennoch über Ordnung und Orte der Dinge
+          sprechen: Universalmöbel: Die Truhe ist im Mittelalter und in der
+          Frührenaissance ein Universalmöbel, in dem alle Besitztümer
+          undifferenziert Platz finden.
+        </p>
+      </AppBox>
+      <AppBox width="small">
+        <p>
+          Vorratskeller: Ein natürlich-klimatisch konditionierter
+          Aufbewahrungsraum für Lebensmittel, welcher im 20. Jhd. abgelöst wird
+          durch einen technisch-klimatisch konditionierten Aufbewahrungsraum:
+          den Kühlschrank.
+        </p>
+      </AppBox>
+      <AppBox width="small">
+        <p>
+          Compactum Wardrobe: Ein Kleiderschrank von 1937 mit unzähligen
+          kleineren Fächern, der als Ausdruck des kulturelles und
+          wissenschaftlichen Bedürfnis nach Klassifizierung und Standardisierung
+          seit dem 18. Jahrhundert betrachtet werden kann, welches im 20.
+          Jahrhundert den Haushalt erreicht.
+        </p>
+      </AppBox>
+      <AppBox width="medium">
+        <p>
+          Das Berliner Mietshaus (hier exemplarisch: Baujahr 1906, mein eigener
+          Wohnort) ist ein generischer Bautyp des 19. und 20. Jahrhunderts: Im
+          Regelfall ohne Architekt*in errichtet, bestimmt durch Parzelle und
+          Bauordnung.
+        </p>
+      </AppBox>
+      <AppBox width="medium">
+        <p>
+          Das Berliner Mietshaus hat einen Nebenraum, viel eher ein
+          Nebenraumgefüge: der Keller. Preußische Kappendecken,
+          Stahlsteindecken, gemauerte Schornsteine. Bindeglied zwischen
+          städtischer Infrastruktur und häuslicher Infrastruktur.
+        </p>
+      </AppBox>
+      <AppBox width="medium">
+        <p>
+          Während es für oben – z. B. die Fassade – eine Bauordnung gibt sowie
+          einen Wunsch nach Repräsentation, bestehen für den Keller informelle
+          Regeln der Gestaltung: Die Keller Konvention.
+        </p>
+      </AppBox>
+
+      <AppBox image="/nebenraum/halbzeug.jpg" width="small"></AppBox>
+      <AppBox image="/nebenraum/tür.jpg" width="small">
+        <p>
+          Das formale Repertoire des Kellers umfasst dabei im Einklang mit der
+          Keller Konvention verschiedene Elemente zur Abgrenzungen: Halbzeuge,
+          Gitter, Paneele, Lochziegel.
+        </p>
+      </AppBox>
+      <AppBox image="/nebenraum/scharnier.jpg" width="small">
+        <p>
+          Aus dem formalen Repertoire ergeben sich Grundelemente und Grundmaße:
+          1. T-Profile, die einfach an die Höhentopographie des Kellers
+          angepasst werden können, 2. kreuzweise ausgesteifte Türen mit 90 cm
+          Breite, 3. kleinere kreuzweise ausgesteifte Türen mit 80 cm Breite.
+          Während die Türen festen Abständen folgen, können die Zwischenabstände
+          mit den T-Profile beliebig variiert werden: Zwang und Spiel.
+        </p>
+      </AppBox>
+      <AppBox width="medium">
+        <p>
+          Im Zwang und Spiel dieses Systems sind dabei neben rechten Winkeln
+          vielfältige Freiformen und Verkleidungskombinationen möglich – hier
+          durchexerziert als Fingerübung.
+        </p>
+      </AppBox>
+      <AppBox width="large">
+        <p>
+          Das neue Raumprogramm bildet sich im Einklang mit der Keller
+          Konvention und dem formalen Repertoire aus. Die Organisation des
+          Grundrisses wirft dabei Fragen auf, weil Zeit im Keller entzerrt ist,
+          Handlungen selten täglich passieren, eher alle paar Tage, alle paar
+          Wochen. Es gibt, anders als in der Wohnung, wenige konkrete
+          Handlungen, keine regelmäßige Zirkulation durch alle Räume. Die Idee
+          der Neuordnung ist es, den Korridor aufzulösen und verschiedene Wege
+          zu ermöglichen durch mehrere Öffnungen. Es geht bei dieser Neuordnung
+          um das definieren von spezifischen Raumfiguren als eine Art poetischer
+          Überhöhung der Konventionen und der Bauteile des Kellers. Jeder Raum
+          ist ein Behälter, der die Dinge, die in ihm enthalten sind, auf eine
+          eigenständige Art und Weise präsentiert, sichtbar macht, ihr
+          Verhältnis rahmt.
+        </p>
+      </AppBox>
+      <AppBox image="/nebenraum/unantastbarkeit.jpg" width="small">
+        <p>
+          <em>Raum der Unantastbarkeit.</em> Durchlaufen, kontrollieren, warten,
+          abdrehen, reparieren, aufdrehen, Zugriff jederzeit. Der Arterien des
+          Hauses. Leitungsgeflechte, die zwar nur im Keller sicht- und hörbar
+          sind, dessen Medien – Strom, Wasser, Gas – jedoch nur in den Wohnungen
+          verfügbar sind.
+        </p>
+      </AppBox>
+      <AppBox width="small">
+        <p>
+          <em>Raum der Zirkulation, Raum der häufigen Frequentierung.</em> Das
+          was man nicht mehr braucht, was in den kollektiven Hausrat übergeht,
+          was neue Besitzer im Haus finden darf. Zum mitnehmen. Der Raum der
+          schnellen Zirkulation, der zeitliche Wetter- und Sicherheitsschutz.
+          Schutzräume für Fahrräder und andere rollende Dinge.
+        </p>
+      </AppBox>
+      <AppBox>
+        <p>
+          <em>Raum der Sedimente.</em> Das was mehr oder weniger achtlos in den
+          Keller gestellt wird. Das wovon man sich vorstellen könnte, dass man
+          es mal wieder brauchen könnte, aber nicht genau weiß wann.
+          Zwischenwelt der Dinge.
+        </p>
+      </AppBox>
+      <AppBox width="small">
+        <p>
+          <em>
+            Raum für Ersatz und Ersetztes, Raum für bearbeitende Dinge, Raum der
+            Verarbeitung.
+          </em>
+          Einsortieren, durchsuchen, rausnehmen, überprüfen, zurechtschneiden,
+          mitnehmen. Dinge und Handlungen, die zum Unterhalt des Hauses
+          notwendig sind. Materielle Basis.
+        </p>
+      </AppBox>
+      <AppBox image="/nebenraum/kuriositäten.jpg" width="small">
+        <p>
+          <em>Raum für Kuriositäten.</em> Die Wunderkammer des Hauses, in dem
+          alle Bewohnende des Hauses ein Objekt stellen müssen. Ein
+          Sammelsurium. Das kollektiv eingerichtete Zimmer, das Bewusstsein des
+          Hauses. Tradition Berliner Mietshäuser seit Beginn des 21.
+          Jahrhunderts.
+        </p>
+      </AppBox>
+      <AppBox image="/nebenraum/geschlossene-behältnisse.jpg" width="medium">
+        <p>
+          <em>Raum für geschlossene Behältnisse.</em>
+          Das sortierte oder das, was man vielleicht nicht zeigen möchte, was
+          man es als zu privat empfindet. Das was noch einem direkten Zugriff
+          aus der Wohnung standhält, weil man genau weiß, wo es ist. Ordnung.
+          <em>Raum des ungestörten Entpackens.</em> Der Raum, in dem die
+          geschlossenen Behältnisse geöffnet werden, in Erinnerungen geschwelgt
+          wird, sortiert wird, die Zukunft der Dinge bestimmt wird: wieder hoch
+          in die Wohnung, veräußert, oder verlagert in andere Räume des Kellers.
+        </p>
+      </AppBox>
+    </template>
+  </AppProject>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
