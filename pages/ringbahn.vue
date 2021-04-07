8e17a30db041a721180f4ac0f123815c196e011f
Felix Deiters <messismore@users.noreply.github.com>
2021-04-07 13:36:21 +0200

    Add ringbahn.vue

---
+++ pages/ringbahn.vue
@@ -0,0 +1,253 @@
+<template>
+  <AppProject
+    name="walking (the ringbahn) as an aesthetic practice"
+    author="Chloé Borreguero"
+  >
+    <template #header>
+      <div>
+        <p>
+          The Ringbahn is an iconic, political, and cultural element of the city
+          of Berlin. Daily used by around 400.000 Berliners, it defines the
+          geography of the city throughout its precise one-hour ride. The
+          Ringbahn line sets a limit between the inner and the outer city,
+          connecting as well as excluding. This project is interested in
+          exploring these contradictions by altering essentially one of its
+          intrinsic factors: speed.
+        </p>
+        <p>
+          This walking experiment takes place between the built city and the
+          urban voids. In this space of encounter, walking becomes the tool with
+          which to approach this marginal landscape, by recognizing geographies
+          in the chaos of peripheries, and inventing new narratives for unknown
+          territories.
+        </p>
+        <p>
+          What happens on the margins of a highly frequented infrastructure?
+          What type of landscapes are spontaneously produced by the entropy of
+          the city? How can I explain the complex geography of a city like
+          Berlin from my own perceptions?
+        </p>
+      </div>
+    </template>
+    <template #main>
+      <AppBox
+        autoplay
+        loop
+        muted
+        video="/ringbahn/portada-1080.mp4"
+        width="large"
+      >
+        <p>
+          ‘The aim is to indicate walking as an aesthetic tool capable of
+          describing and modifying those metropolitan spaces that often have a
+          nature still demanding comprehension, to be filled with meanings
+          rather than designed and filled with things. Walking then turns out to
+          be a tool which, precisely due to the simultaneous reading and writing
+          of space intrinsic to it, lends itself to attending to and interacting
+          with the mutability of those spaces, so as to intervene in their
+          continuous becoming by acting in the field, in the here and now of
+          their transformation, sharing from the inside in the mutations of
+          these spaces that defy the conventional tools of contemporary design.’
+          <em
+            >Careri, Francesco (2002). Walkscapes: walking as an aesthetic
+            practice.</em
+          >
+        </p>
+      </AppBox>
+      <AppBox width="medium" image="/ringbahn/manifesto-fiction-situated.jpg">
+        <p>
+          <span class="nowrap">1. enter /</span>
+          <span class="nowrap">2. cross /</span>
+          <span class="nowrap">3. perceive /</span>
+          <span class="nowrap">4. deposit testimony</span>
+        </p>
+      </AppBox>
+      <AppBox width="medium" image="/ringbahn/no-trespassing.jpg">
+        <p>Walking is not a welcome activity on the Ringbahn.</p>
+      </AppBox>
+      <AppBox
+        autoplay
+        loop
+        muted
+        video="/ringbahn/actions-1080.mp4"
+        width="medium"
+      >
+        <p>
+          This list contains a series of actions that can be a useful aesthetic
+          tool with which to explore, transform and activate the hybrid
+          territories of the contemporary city. These actions can be combined in
+          14.792 different ways. The active subjects are to challenge their
+          creativity and follow their intuitions and to play physically with
+          these concepts, challenging the possibilities offered by the
+          surrounding environment.
+        </p>
+      </AppBox>
+      <AppBox width="small">
+        <p>
+          Walking along the Ringbahn generates tensions. it is a political and
+          cultural act. it is not just wandering, having a walk, strolling...
+          this type of walking generates conflict.
+        </p>
+      </AppBox>
+      <AppBox width="small">
+        <p>
+          ‘It was by walking that man began to construct the natu- ral landscape
+          of his surroundings. and in our own century we have formulated the
+          categories for interpreting the urban landscapes that surround us by
+          walking through them.’
+          <em
+            >Careri, Francesco (2002). Walkscapes: walking as an aesthetic
+            practice.</em
+          >
+        </p>
+      </AppBox>
+      <AppBox width="small">
+        <p>
+          The atlas aims to create a visual narrative of the landscape of the
+          Ringbahn, understood as an ecology in itself, with a system of
+          relations between different elements and patterns across the territory
+          which can be associated, identified and catalogued.
+        </p>
+      </AppBox>
+      <AppBox
+        autoplay
+        loop
+        muted
+        video="/ringbahn/scan-1-1080.mp4"
+        width="medium"
+      >
+      </AppBox>
+      <AppBox
+        autoplay
+        loop
+        muted
+        video="/ringbahn/scan-2-1080.mp4"
+        width="medium"
+      >
+        <p>
+          Journal impressions. The archive of experiences is the only form of
+          mapping possible for these territories.
+        </p>
+      </AppBox>
+      <AppBox width="small" image="/ringbahn/fissure.jpg">
+        <p>
+          A crack, a fissure, reveals an opportunity, the possibility that
+          something can happen, that there is space for other realities. When
+          trespassing these physical frontiers one becomes automatically a
+          marginal body, a part of the landscape.
+        </p>
+      </AppBox>
+      <AppBox width="small" image="/ringbahn/menhir.jpg">
+        <p>
+          Prehistorical menhirs, as described by F. Careri, gave information
+          about the ‘geography of the place’. Perhaps in the super- modernity
+          era specific geography is not so important anymore. The conditions of
+          space are dictated by economic reasons. Along the Ringbahn, the visual
+          landscape is filled with contemporary versions of the prehistorical
+          menhir. These can be found under the shape of billboards, on
+          ‘Brandwände’, or as pilled up containers. Advertising determines the
+          image of the city, especially along the Ringbahn in the periphery.
+        </p>
+      </AppBox>
+      <AppBox width="small" image="/ringbahn/graffiti-1.jpg"> </AppBox>
+      <AppBox width="small" image="/ringbahn/graffiti-2.jpg">
+        <p>
+          Prehistorical menhirs, as described by F. Careri, gave information
+          about the ‘geography of the place’. Perhaps in the super- modernity
+          era specific geography is not so important anymore. The conditions of
+          space are dictated by economic reasons. Along the Ringbahn, the visual
+          landscape is filled with contemporary versions of the prehistorical
+          menhir. These can be found under the shape of billboards, on
+          ‘Brandwände’, or as pilled up containers. Advertising determines the
+          image of the city, especially along the Ringbahn in the periphery.
+        </p>
+      </AppBox>
+      <AppBox width="small" image="/ringbahn/train.jpg">
+        <p>
+          The landscape of the Ringbahn is a hybrid. it presents variations in
+          topography such as mountains, valleys, hydrographies, hills, paths...
+        </p>
+      </AppBox>
+      <AppBox width="small" image="/ringbahn/landscape-1.jpg">
+        <p>
+          These artificial topographies are the testimony of the different
+          layers of transformation through time, of excavations and moving of
+          piles of earth. Concrete debris and infrastructural elements are read
+          as geological elements (mountains, paths, etc).
+        </p>
+      </AppBox>
+      <AppBox width="small" image="/ringbahn/landscape-2.jpg">
+        <p>
+          The natural elements found in the margins of the ringbahn be- long to
+          the unplanned, they become resilient to the mistreat- ment of the
+          earth and find ways to reappropriate their space.
+        </p>
+      </AppBox>
+      <AppBox width="small" image="/ringbahn/landscape-3.jpg">
+        <p>
+          They are the physical result of an infrastructure cutting through the
+          territory.
+        </p>
+      </AppBox>
+      <AppBox width="small" image="/ringbahn/excavator.jpg">
+        <p>
+          ‘(...) It was hard to tell the new highway from the old road; they
+          were both confounded into a unitary chaos. Since it was Saturday, many
+          machines were not working, and this caused them to resemble
+          prehistoric creatures trapped in the mud, or, better, extinct
+          machines—mechanical dinosaurs stripped of their skin.’
+          <em>R.Smithson (1967): A Tour of the Monuments of Passaic</em>
+        </p>
+      </AppBox>
+      <AppBox width="small">
+        <p>
+          If we understand entropy as what we leave as unproductive in terms of
+          energy balance, we will realize that what we call the margin
+          constitutes an ideal receptacle in which to place everything that is
+          not directly useful to the more or less organized configuration of the
+          city.
+        </p>
+        <p>
+          The blurred limits of the urban, the peri-urban space, that have
+          neither the vocation nor the capacity to offer the specific con-
+          ditions of the urban, by default become catalysts of everything that
+          we do not want in our cities: large transport infrastructures,
+          industries, distribution, and logistic zones of large scale, and all
+          the possible forms of civil engineering and debris.
+        </p>
+        <p>
+          The entropic landscapes are spaces in which disorderly, un- planned
+          elements gradually accumulate all sorts of worthless, impersonal,
+          noisy and undesirable constructions, infrastructures and objects.
+        </p>
+      </AppBox>
+      <AppBox width="small" image="/ringbahn/backyard-1.jpg"> </AppBox>
+      <AppBox width="small" image="/ringbahn/backyard-2.jpg"> </AppBox>
+      <AppBox class="prose" width="medium">
+        <p>
+          The content available on the digital archive of the Ringbahn aims to
+          offer a framework and a set of resources for walking, exploring and
+          understanding the landscape. The conducted research is an on-going
+          project and is therefore always subject to alterations and
+          collaborations. Although the content tries to be broad enough to cover
+          the spectre of the Ringbahn’s elements, it is created from my own
+          perceptions and subjectivities and may therefore be reappropriated and
+          redefined by other walkers.
+        </p>
+        <p>FOLLOW A LINE, START WALKING.</p>
+      </AppBox>
+      <AppBox width="large">
+        <a
+          href="https://urbaniterations.org/walking-the-ringbahn-as-an-aesthetic-practice/"
+          >UrbanIterations.org</a
+        >
+      </AppBox>
+    </template>
+  </AppProject>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
